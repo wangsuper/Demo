755821002b3463d6e5c86a17fbfb2d16daeab292
wangsuper <472395480@qq.com>
2020-07-25 20:24:32 +0800

    添加get/set

--- src/main/java/com/example/demo/TestDemo1.java
+++ src/main/java/com/example/demo/TestDemo1.java
@@ -13,4 +13,19 @@ public class TestDemo1 {
     private int age;
 
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
