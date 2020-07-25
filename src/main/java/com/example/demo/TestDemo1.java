fce368768c4177e6f4b29b40c0f327f00798e875
wangsuper <472395480@qq.com>
2020-07-25 20:21:41 +0800

    添加第一个类

---
+++ src/main/java/com/example/demo/TestDemo1.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+/**
+ * <p></p>
+ *
+ * @author wangc
+ * @date Created in 2020-07-25 20:20
+ */
+public class TestDemo1 {
+
+    private String name;
+
+    private int age;
+
+
+}
