d0f4378a388fc9fb6d6fe421a6c3a4f4e76b22d1
wangsuper <wc13823372590>
2020-08-07 20:43:07 +0800

    2

--- src/main/java/com/example/demo/TestDemo1.java
+++ src/main/java/com/example/demo/TestDemo1.java
@@ -10,7 +10,7 @@ public class TestDemo1 {
 
     private String age;
 
-    private int address;
+    private int address2222;
 
     private int sex;
 
@@ -22,11 +22,11 @@ public class TestDemo1 {
         this.age = age;
     }
 
-    public int getAddress() {
-        return address;
+    public int getAddress2222() {
+        return address2222;
     }
 
-    public void setAddress(int address) {
-        this.address = address;
+    public void setAddress2222(int address2222) {
+        this.address2222 = address2222;
     }
 }
